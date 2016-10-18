d7aeb3796276c34cbffd568adf43f11e9990bb21
Alexey <reshetyk@gmail.com>
2016-10-19 00:33:05 +0200

    sample of calling rest service

--- src/main/java/example/MySpringBootRouter.java
+++ src/main/java/example/MySpringBootRouter.java
@@ -1,6 +1,10 @@
 package example;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.apache.camel.component.restlet.RestletComponent;
 import org.apache.camel.spring.boot.FatJarRouter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.HealthEndpoint;
@@ -16,23 +20,64 @@ public class MySpringBootRouter extends FatJarRouter {
     @Autowired
     private CamelContext camelContext;
 
-    @Override
-    public void configure() {
-//        camelContext.addComponent("activemqLoc", activeMQComponent("vm://localhost?broker.persistent=false"));
+    @Autowired
+    private SomeService someService;
 
-        from("activemq:queue:test_queue")
-                .transform().simple("ref:myBean")
-                .to("log:out");
-
-        from("timer:status")
-                .bean(health, "invoke")
-                .log("Health is ${body}");
-
+//    @Bean
+//    public RestConfiguration restConfiguration() {
+//      return new RestConfiguration();
+//    }
+    @Bean
+    public RestletComponent restlet() {
+        return new RestletComponent();
     }
 
     @Bean
-    String myBean() {
-        return "I'm Spring bean!";
+    public RouteBuilder restRoute () {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                restConfiguration().component("restlet").port(8091);
+                rest("/say")
+                        .get("/hello").produces("text/html").to("direct:hello")
+                        .get("/bye").consumes("application/json").to("direct:bye")
+                        .post("/bye").to("mock:update");
+
+                from("direct:hello")
+                        .log(LoggingLevel.INFO, "ok!!!")
+                        .transform().constant("<!DOCTYPE html>\n<html><body><h1>Hello World</h1></body></html>");
+                from("direct:bye")
+                        .transform().constant("Bye World");
+            }
+        };
+    }
+
+
+//    @Bean
+    public RouteBuilder route1 () {
+
+        RouteBuilder routeBuilder = new RouteBuilder() {
+            JacksonDataFormat json = new JacksonDataFormat(SomeDomain.class);
+
+            @Override
+            public void configure() throws Exception {
+                from("timer:status?period=10000")
+                        .unmarshal(json)
+                        .bean(someService, "test()")
+                        .to("log:out");
+            }
+        };
+
+        routeBuilder
+                .onException(Exception.class)
+                .log(LoggingLevel.ERROR, "--------\n" + "${header.EXCEPTION_CAUGHT}" + " \n--------the bug --------");
+
+        return routeBuilder;
+    }
+
+    @Bean
+    public SomeService someService() {
+        return new SomeService();
     }
 
 }
--- src/main/java/example/SampleSpringBootService.java
+++ src/main/java/example/SampleSpringBootService.java
@@ -1,21 +1,14 @@
 package example;
 
-import io.hawt.config.ConfigFacade;
 import io.hawt.springboot.EnableHawtio;
-import io.hawt.springboot.HawtPlugin;
-import io.hawt.springboot.PluginService;
-import io.hawt.system.ConfigManager;
 import io.hawt.web.AuthenticationFilter;
 import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.broker.TransportConnector;
 import org.apache.activemq.store.kahadb.KahaDBPersistenceAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import javax.annotation.PostConstruct;
-import javax.servlet.ServletContext;
 import java.io.File;
 import java.net.URI;
 
@@ -23,54 +16,54 @@ import java.net.URI;
 @EnableHawtio
 public class SampleSpringBootService {
 
-    @Autowired
-    private ServletContext servletContext;
+//    @Autowired
+//    private ServletContext servletContext;
 
     public static void main(String[] args) {
         System.setProperty(AuthenticationFilter.HAWTIO_AUTHENTICATION_ENABLED, "false");
         SpringApplication.run(SampleSpringBootService.class, args);
     }
 
-    @PostConstruct
-    public void init() {
-        final ConfigManager configManager = new ConfigManager();
-        configManager.init();
-        servletContext.setAttribute("ConfigManager", configManager);
-    }
+//    @PostConstruct
+//    public void init() {
+//        final ConfigManager configManager = new ConfigManager();
+//        configManager.init();
+//        servletContext.setAttribute("ConfigManager", configManager);
+//    }
 
     /**
      * Loading an example plugin
      * @return
      */
-    @Bean
-    public HawtPlugin samplePlugin() {
-        return new HawtPlugin("sample-plugin", "/hawtio/plugins", "", new String[] { "sample-plugin/js/sample-plugin.js" });
-    }
+//    @Bean
+//    public HawtPlugin samplePlugin() {
+//        return new HawtPlugin("sample-plugin", "/hawtio/plugins", "", new String[] { "sample-plugin/js/sample-plugin.js" });
+//    }
 
     /**
      * Set things up to be in offline mode
      * @return
      * @throws Exception
      */
-    @Bean
-    public ConfigFacade configFacade() throws Exception {
-        ConfigFacade config = new ConfigFacade() {
-            public boolean isOffline() {
-                return true;
-            }
-        };
-        config.init();
-        return config;
-    }
+//    @Bean
+//    public ConfigFacade configFacade() throws Exception {
+//        ConfigFacade config = new ConfigFacade() {
+//            public boolean isOffline() {
+//                return true;
+//            }
+//        };
+//        config.init();
+//        return config;
+//    }
 
     /**
      * Register rest endpoint to handle requests for /plugin, and return all registered plugins.
      * @return
      */
-    @Bean
-    public PluginService pluginService(){
-        return new PluginService();
-    }
+//    @Bean
+//    public PluginService pluginService(){
+//        return new PluginService();
+//    }
 
     @Bean
     public BrokerService brokerService () throws Exception {
